54cd7fd6c8076af76ade7d20d01b10e954b0bfd7
theabhishekkumarsingh <theabhishekkr.singh@gmail.com>
2024-12-21 19:36:59 +0530

    Update MultiplexerA.v

--- MultiplexerA.v
+++ MultiplexerA.v
@@ -1,9 +1,18 @@
 // Multiplexer (A) - also known as data selector 
-module muxA(in1, in2, sel, outA);
+module muxA(clk,in1, in2, sel, outA);
+input clk;
 input sel;
 input [18:0]in1;
 input [18:0]in2;
-output [18:0]outA;
+output reg [18:0]outA;
 
-assign outA = (sel == 1)? in1 : in2; // implemented using ternary operator
+always@(posedge clk)
+begin
+    if (sel == 1)begin
+        outA <= in1;
+    end
+    else if (sel == 0)begin
+        outA <= in2;
+    end
+end
 endmodule
