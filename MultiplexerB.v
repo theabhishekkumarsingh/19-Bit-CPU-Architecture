f8092876cad521b7a5e1da7e14e8c846359c69a2
theabhishekkumarsingh <theabhishekkr.singh@gmail.com>
2024-12-21 19:37:35 +0530

    Update MultiplexerB.v

--- MultiplexerB.v
+++ MultiplexerB.v
@@ -1,9 +1,17 @@
 // Multiplexer (B) = also known as data selector 
-module muxB(in1, in2, sel, outB);
+module muxB(clk, in1, in2, sel, outB);
+input clk;
 input sel;
 input [13:0]in1;
 input [13:0]in2;
-output [13:0]outB;
+output reg [13:0]outB;
 
-assign outB = (sel == 1)? in1 : in2; // implemented using ternary operator 
+always@(posedge clk)begin
+    if(sel == 1)begin
+        outB <= in1;
+    end
+    else if (sel == 0)begin
+        outB <= in2;
+    end
+end 
 endmodule
